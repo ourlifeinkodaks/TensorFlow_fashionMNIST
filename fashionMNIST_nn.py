6c7167d9a0779f2388d1c7e18e3256cd7b3d4e26
raveesh1 <raveesh@tatvic.com>
2019-03-15 18:20:30 +0530

    CNN code added

--- fashionMNIST_nn.py
+++ fashionMNIST_nn.py
@@ -49,18 +49,19 @@ plt.colorbar()
 plt.grid(False)
 plt.show()
 
-# Display the first 25 images from the dataset and their classes
-plt.figure(figsize=(10, 10))
+plt.figure(figsize=(10,10))
 i = 0
 for (image, label) in test_dataset.take(25):
-	image = image.numpy().reshape((28, 28))
-	plt.subplot(5, 5, i+1)
-	plt.xticks([])
-	plt.yticks([])
-	plt.grid(False)
-	plt.imshow(image, cmap=plt.cm.binary)
-	plt.xlabel(class_names[label])
+    image = image.numpy().reshape((28,28))
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(image, cmap=plt.cm.binary)
+    plt.xlabel(class_names[label])
+    i += 1
 plt.show()
+
 
 # Neural network
 model = tf.keras.Sequential([tf.keras.layers.Flatten(input_shape=[28, 28, 1]),
@@ -74,7 +75,7 @@ BATCH_SIZE = 32
 train_dataset = train_dataset.repeat().shuffle(num_train_examples).batch(BATCH_SIZE)
 test_dataset = test_dataset.batch(BATCH_SIZE)
 
-model.fit(train_dataset, epochs=5, steps_per_epoch=math.ceil(num_train_examples/BATCH_SIZE))
+model.fit(train_dataset, epochs=10, steps_per_epoch=math.ceil(num_train_examples/BATCH_SIZE))
 
 # Testing the Model
 test_loss, test_accuracy = model.evaluate(test_dataset, steps=math.ceil(num_test_examples/32))
@@ -92,3 +93,46 @@ predictions[0]
 np.argmax(predictions[0])
 test_labels[0]
 
+# Graph of predictions
+def plot_image(i, predictions_array, true_labels, images):
+  predictions_array, true_label, img = predictions_array[i], true_labels[i], images[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  
+  plt.imshow(img[...,0], cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+  
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array[i], true_label[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1]) 
+  predicted_label = np.argmax(predictions_array)
+ 
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
+# Plot the first X test images, their predicted label, and the true label
+# Color correct predictions in blue, incorrect predictions in red
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions, test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions, test_labels)
